7a34fb499d01a51ff9e1c3313a1abca0b7f40e8d
Elsa2116 <elsialemayehu33@gmail.com>
2025-03-23 12:10:09 -0400

    done

--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,21 @@
 // Iteration #1
+const mongoose = require("mongoose");
+const Drone = require("../models/Drone.model");
+
+const drones = [
+  { name: "Creeper XL 500", propellers: 3, maxSpeed: 12 },
+  { name: "Racer 57", propellers: 4, maxSpeed: 20 },
+  { name: "Courier 3000i", propellers: 6, maxSpeed: 18 }
+];
+
+mongoose
+  .connect("mongodb://localhost:27017/dronesDB")
+  .then(() => {
+    console.log("Connected to MongoDB");
+    return Drone.create(drones);
+  })
+  .then((dronesCreated) => {
+    console.log(`Seeded ${dronesCreated.length} drones`);
+    mongoose.connection.close();
+  })
+  .catch((err) => console.log("Error seeding data:", err));
