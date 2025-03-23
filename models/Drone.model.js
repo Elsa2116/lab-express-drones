55c407aab7a811cceab0cae986bbf30db6d7f7c4
Elsa2116 <elsialemayehu33@gmail.com>
2025-03-23 12:11:10 -0400

    done

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,12 @@
+const mongoose = require("mongoose");
+
+const droneSchema = new mongoose.Schema({
+  name: String,
+  propellers: Number,
+  maxSpeed: Number,
+});
+
+const Drone = mongoose.model("Drone", droneSchema);
+
+module.exports = Drone;
 // Iteration #1
